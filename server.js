de44a40f1a02a3b1e5954ec222587980bb6ba5e6
Rodrigo Tufino <rtufino@ups.edu.ec>
2022-01-10 08:51:56 -0500

    Prueba

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
     res.render('home', {
         titulo: 'Home',
-        nombre: "ariEL"
+        nombre: "ariELito Fonseca"
     });
 });
 
